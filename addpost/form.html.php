9dfa476366f01d1bf3991676beb89f153b5a25b6
Fadee Ivan <34217001+IFadeev@users.noreply.github.com>
2018-02-03 18:51:05 +0300

    Add files via upload

---
+++ addpost/form.html.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="UTF-8">
+    <title>Add post!</title>
+    <style>
+        div {
+            margin: 10px;
+        }
+    </style>
+</head>
+<body>
+<form action="?" method="post">
+    <div>
+        <label for ="artcl-title">Название статьи:</label>
+        <input type="text" name="artcl-title" id="artcl-title" placeholder="Название статьи">
+    </div>
+    <div>
+        <label for="ctgr">Категория</label>
+        <select name="ctgr" id="ctgr">
+            <option>1</option>
+            <option>2</option>
+            <option>3</option>
+        </select>
+    </div>
+    <div>
+        <label for ="addtext"></label>
+        <textarea name="text" id="addtext" rows="7" cols="50" placeholder="Введите текст"></textarea>
+    </div>
+    <div>
+        <input type="submit" value="Добавить">
+    </div>
+</form>
+</body>
+</html>
---
+++ addpost/index.php
@@ -0,0 +1,80 @@
+ <?php
+ $dsn = 'mysql:host=localhost;dbname=test_bd';
+ $username = 'test_user_artcls';
+ $passwd = 'V4azBSHosdBQBQHv';
+
+ if (get_magic_quotes_gpc())
+ {
+     $process = array(&$_GET, &$_POST, &$_COOKIE, &$_REQUEST);
+     while (list($key, $val) = each($process))
+     {
+         foreach ($val as $k => $v)
+         {
+             unset($process[$key][$k]);
+             if (is_array($v))
+             {
+                 $process[$key][stripslashes($k)] = $v;
+                 $process[] = &$process[$key][stripslashes($k)];
+             }
+             else
+             {
+                 $process[$key][stripslashes($k)] = stripslashes($v);
+             }
+         }
+     }
+     unset($process);
+ }
+ if (isset($_GET['addpost'])) {
+     include 'form.html.php';
+     exit();
+ }
+ try
+ {
+     $pdo = new PDO($dsn, $username, $passwd);
+     $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+     $pdo->exec('SET NAMES "utf8"');
+ }
+ catch (PDOException $e)
+ {
+     $error = 'Невозможно подключиться к серверу баз данных.';
+     include 'error.html.php';
+     exit();
+ }
+     if (isset($_POST['addtext'])) {
+         try {
+             $sql = 'INSERT INTO articles SET 
+                 id = NULL,
+                   artcle_titl = :artcl-title,
+                 artcle_text = :text,
+                 categoria_id = :ctgr,
+                 post_date = NOW(),
+                 views = "0" ';
+             $s = $pdo->prepare($sql);
+             $s->bindValue('');
+             }
+             catch (PDOException $e) {
+             $error = "There's no connection to DB" . '<br>' .
+                 $e->getMessage();
+             include 'error.html.php';
+             exit();
+         }
+         header('Location: .');
+         exit();
+     }
+ try {
+     $sql = 'SELECT * FROM `articles`';
+     $result = $pdo->query($sql);
+ }
+ catch (PDOException $e) {
+     $error = "There's no connection to DB" . '<br>' .
+         $e->getMessage();
+     include 'error.html.php';
+     exit();
+ }
+ foreach ($result as $row) {
+     $output[] = $row['post_date'];
+     $output[] = $row['artcle_title'];
+     $output[] = $row['artcle_text'];
+ }
+
+ include 'output.html.php';
