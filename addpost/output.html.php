65b4439b29448300ba0505207cf3d3f96f3a6bd9
Fadee Ivan <34217001+IFadeev@users.noreply.github.com>
2018-02-06 23:46:26 +0300

    Update output.html.php

--- addpost/output.html.php
+++ addpost/output.html.php
@@ -7,10 +7,10 @@
 <body>
 <p><a href="?addpost">Добавить статью</a></p>
 <p>Публикации:</p>
-    <?php foreach ($output as $item): ?>
+    <?php foreach ($output as $data): ?>
     <blockquote>
         <p>
-        <?php echo htmlspecialchars($item, ENT_QUOTES, 'UTF-8');?>
+        <?php echo htmlspecialchars($data, ENT_QUOTES, 'UTF-8');?>
         </p>
     </blockquote>
     <?php endforeach; ?>
