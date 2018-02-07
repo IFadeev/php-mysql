df9d6f99071bd2d8f0b68e075f0fe89c6cd566b4
Fadee Ivan <34217001+IFadeev@users.noreply.github.com>
2018-02-08 00:13:05 +0300

    Update output.html.php

--- post/output.html.php
+++ post/output.html.php
@@ -8,18 +8,19 @@
 <p><a href="?addpost">Добавить статью</a></p>
 <p>Публикации:</p>
 <?php foreach ($output as $data): ?>
-    <blockquote>
-        <?php echo '<p>'. htmlspecialchars($data['post_date'], ENT_QUOTES, 'utf-8') .'</p>';
-        echo '<p>'. htmlspecialchars($data['artcle_title'], ENT_QUOTES, 'utf-8') .'</p>' ?>
-        <p> Автор: <a href="mailto:<?php echo htmlspecialchars($data['author_email'],
-                ENT_QUOTES, 'utf-8');?>">
-                <?php echo htmlspecialchars($data['author_name'], ENT_QUOTES, 'utf-8');?></a>
-        </p>
-        <?php echo '<p>'. htmlspecialchars($data['artcle_text'], ENT_QUOTES, 'utf-8') .'</p>'?>
-        <input type="hidden" name="id" value="<?php echo $data['id'];?>">
-        <input type="submit" value="Удалить cтатью">
-    </blockquote>
+    <form action="?deletepost" method="post">
+        <blockquote>
+            <?php echo '<p>'. htmlspecialchars($data['post_date'], ENT_QUOTES, 'utf-8') .'</p>';
+                  echo '<p>'. htmlspecialchars($data['artcle_title'], ENT_QUOTES, 'utf-8') .'</p>' ?>
+            <p> Автор: <a href="mailto:<?php echo htmlspecialchars($data['author_email'],
+                    ENT_QUOTES, 'utf-8');?>">
+                    <?php echo htmlspecialchars($data['author_name'], ENT_QUOTES, 'utf-8');?></a>
+            </p>
+            <?php echo '<p>'. htmlspecialchars($data['artcle_text'], ENT_QUOTES, 'utf-8') .'</p>'?>
+            <input type="hidden" name="id" value="<?php echo $data['id'];?>">
+            <input type="submit" value="Удалить cтатью">
+        </blockquote>
+    </form>
 <?php endforeach;?>
 </body>
-
 </html>
