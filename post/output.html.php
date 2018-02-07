3af3ffe689e6c530120e3e494d01d2f17be7dba2
Fadee Ivan <34217001+IFadeev@users.noreply.github.com>
2018-02-07 16:23:05 +0300

    Update output.html.php

--- post/output.html.php
+++ post/output.html.php
@@ -7,13 +7,19 @@
 <body>
 <p><a href="?addpost">Добавить статью</a></p>
 <p>Публикации:</p>
-    <?php foreach ($output as $data): ?>
+<?php foreach ($output as $data): ?>
     <blockquote>
-        <p>
-        <?php echo htmlspecialchars($data, ENT_QUOTES, 'UTF-8');?>
+        <?php echo '<p>'. htmlspecialchars($data['post_date'], ENT_QUOTES, 'utf-8') .'</p>';
+        echo '<p>'. htmlspecialchars($data['artcle_title'], ENT_QUOTES, 'utf-8') .'</p>' ?>
+        <p> Автор: <a href="mailto:<?php echo htmlspecialchars($data['author_email'],
+                ENT_QUOTES, 'utf-8');?>">
+                <?php echo htmlspecialchars($data['author_name'], ENT_QUOTES, 'utf-8');?></a>
         </p>
+        <?php echo '<p>'. htmlspecialchars($data['artcle_text'], ENT_QUOTES, 'utf-8') .'</p>'?>
+        <input type="hidden" name="id" value="<?php echo $data['id'];?>">
+        <input type="submit" value="Удалить cтатью">
     </blockquote>
-    <?php endforeach; ?>
+<?php endforeach;?>
 </body>
 
 </html>
